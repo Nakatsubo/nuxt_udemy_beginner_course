3f39245fffa8ac486fb995ba6e9b338b51aca60b
YusukeNakatsubo <nakatsubo.works@gmail.com>
2021-11-23 17:29:32 +0900

    add introduction files

---
+++ hello/pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <section class="container">
+    <h1>{{ message }}</h1>
+    <hr>
+    <router-link to="price">Price Page</router-link>
+  </section>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      message: 'Hello, World!'
+    }
+  }
+}
+</script>
