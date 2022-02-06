d2de792ea1357af8b6125143226db543e65eb216
YusukeNakatsubo <nakatsubo.works@gmail.com>
2022-02-06 12:33:22 +0900

    add nuxt-view PJ

---
+++ nuxt-view/pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <section class="container">
+    <p>Hello World</p>
+  </section>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+section { font-family: 'Permanent Marker', cursive; }
+</style>
