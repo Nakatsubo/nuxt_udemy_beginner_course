a15d2a3f9b5fd66f335b708a89427d8e04ddde4c
YusukeNakatsubo <nakatsubo.works@gmail.com>
2022-02-06 13:11:27 +0900

    add nuxt-async PJ

---
+++ nuxt-async/pages/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <section class="container">
+    <div>
+      <!-- {{ users[0].id }}, {{ users[0].name }} -->
+      <ul>
+        <li v-for="user in users" :key="user.id">
+          {{ user.id }}, {{ user.name }}, {{ user.company.name }}
+        </li>
+      </ul>
+    </div>
+  </section>
+</template>
+
+<script>
+const axios = require('axios');
+let url = 'https://jsonplaceholder.typicode.com/usersxxxxx';
+export default {
+  // コンポーネントを初期化する前に非同期の通信を行えるようにするためのメソッド
+  asyncData({ params, error }) {
+    return axios.get(url)
+      .then((res) => {
+        return { users: res.data }
+      })
+      .catch((e => {
+        // console.log(e.response.status)
+        error({ statusCode: e.response.status, message: e.message })
+      }))
+  }
+}
+</script>
